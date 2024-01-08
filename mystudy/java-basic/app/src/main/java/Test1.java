025aeb43548f8e604564f0e794b0dea2b27b63dc
yusoonsun <tnstjs39@naver.com>
2024-01-08 17:59:37 +0900

    .

---
+++ mystudy/java-basic/app/src/main/java/Test1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test1 {
+  public static void main(String[] args) {
+	  Scanner scanner = new Scanner(System.in);
+	  int a, b;
+	  a = scanner.nextInt();
+	  b = scanner.nextInt();
+	  
+	  System.out.println(a + b);
+	  
+  }
+}
