6a2c954b32e139eb958b392b287a0588ea876679
yusoonsun <tnstjs39@naver.com>
2024-01-25 17:47:27 +0900

    -

--- myapp/app-15/src/main/java/bitcamp/myapp/vo/Board.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.myapp.vo;
-
-public class Board {
-
-  public String title;
-  public String content;
-  public String writer;
-  public String createdDate;
-}
--- myapp/app-17/src/main/java/bitcamp/myapp/vo/Assignment.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.myapp.vo;
-
-public class Assignment {
-
-  public String title;
-  public String content;
-  public String deadline;
-
-}
